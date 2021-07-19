af241333471f0f347980744a04f5eea577c07f47
unknown <you@example.com>
2021-07-19 16:27:13 +0200

    initial commitss

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/<testcheckout>'));
+app.use(express.static('./dist/porterbuddy'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/<testcheckout>/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/porterbuddy/'}),
+);
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
