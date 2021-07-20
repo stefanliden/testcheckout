c3da96d11f63af261abedeeea466014f79a212d4
unknown <you@example.com>
2021-07-20 11:55:53 +0200

    My first commit

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ let app = express();
 app.use(cors());
 app.options('*', cors());
 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-   
-  app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-  })
-  
 
 app.use(express.static(__dirname + '/dist/angular-on-heroku'));
 app.get('/*', function(req,res) {
