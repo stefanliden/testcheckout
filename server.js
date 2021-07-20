2740dd3d6b764faa881a604e661fa66aa49e2ea2
unknown <you@example.com>
2021-07-20 09:48:35 +0200

    My first commit

--- server.js
+++ server.js
@@ -2,8 +2,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-var cors = require('cors')
-app.use(cors())
+
+(function() {
+    var cors_api_host = 'https://porterbuddy.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
 app.use(express.static(__dirname + '/dist/angular-on-heroku'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
