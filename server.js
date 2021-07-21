48b37af3791ec8df97e30a2500c709f340bfd7aa
unknown <you@example.com>
2021-07-21 14:49:29 +0200

    My first commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 var cors = require('cors')
 app.use(cors())
-app.use(express.static(__dirname + '/dist/angular-on-heroku'));
+app.use(express.static(__dirname + '/dist/'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/angular-on-heroku/index.html'));});
+    '/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
