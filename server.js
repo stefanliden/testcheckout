17ce554a168225ec04b38b1bd00ed71cb86af307
unknown <you@example.com>
2021-07-21 14:35:33 +0200

    My first commit

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/<to_your_project_name>'));
-
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/<to_your_project_name>/index.html'));
-});
-// Start the app by listening on the default Heroku port
+var cors = require('cors')
+app.use(cors())
+app.use(express.static(__dirname + '/dist/angular-on-heroku'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/angular-on-heroku/index.html'));});
 app.listen(process.env.PORT || 8080);
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -22,20 +22,22 @@ export class DashboardComponent implements OnInit {
   articleDataBool = false;
   credTestDataBool = false;
   credDataBool = false;
+  apiDataBool = false;
+
   paymentDataBool = false;
   showErrorDataBool = false;
-  
+
   showCredentials = false;
   showCustomer = false;
   showCustomerShipping = false;
-  expandAllBool = false ;
+  expandAllBool = false;
   showIframe = false;
   showSpinner = true;
   debugBool = false;
-  orderID  : any;
+  orderID: any;
   articleDataBool1 = false;
   showIframe1 = false;
-  deliveryWindow : any;
+  deliveryWindow: any;
   url: string = "";
   urlSafe: SafeResourceUrl;
   requestData: string[];
@@ -50,23 +52,20 @@ export class DashboardComponent implements OnInit {
   paymentDataForm: FormGroup;
   checkoutDataForm: FormGroup;
   originAddress: FormGroup;
+  apiKeyForm: FormGroup;
   destinationAddress: FormGroup;
-
   customerFormBilling: FormGroup;
   customerFormShipping: FormGroup;
   customerFormAdressNr: FormGroup;
   articleForm: FormGroup;
   articleForm1: FormGroup;
-
   contactForm: FormGroup;
   orderForm: FormGroup;
-
   selectedBool = false;
   parcels: FormGroup;
   recipient: FormGroup;
   pickupWindows: FormGroup;
-
-saveData : any;
+  saveData: any;
   debugTitle = "Show Debug";
   expandTitle = 'Expand All';
   updateTitle = "InitCheckout"
@@ -89,7 +88,7 @@ saveData : any;
     }
   }
 
-  
+
 
   public expandAll() {
     if (this.expandAllBool == false) {
@@ -99,6 +98,7 @@ saveData : any;
       this.articleDataBool = true;
       this.credTestDataBool = true;
       this.credDataBool = true;
+      this.apiDataBool = true;
       this.paymentDataBool = true;
       this.articleDataBool1 = true;
       this.checkoutDataBool = true;
@@ -111,6 +111,8 @@ saveData : any;
       this.articleDataBool = false;
       this.credTestDataBool = false;
       this.credDataBool = false;
+      this.apiDataBool = false;
+
       this.paymentDataBool = false;
       this.articleDataBool1 = false;
 
@@ -129,6 +131,10 @@ saveData : any;
 
   public articleDataShow(event) {
     if (this.articleDataBool == false) { this.articleDataBool = true; } else { this.articleDataBool = false; }
+  }
+
+  public ApiDataShow(event) {
+    if (this.apiDataBool == false) { this.apiDataBool = true; } else { this.apiDataBool = false; }
   }
   public articleDataShow1(event) {
     if (this.articleDataBool1 == false) { this.articleDataBool1 = true; } else { this.articleDataBool1 = false; }
@@ -160,34 +166,33 @@ saveData : any;
 
   }
 
-  
 
-  createOrder(){
-    console.log(this.deliveryWindow);
+
+  createOrder() {
     let originAddress = JSON.parse(JSON.stringify(this.originAddress.value));
     let recipientAdress = JSON.parse(JSON.stringify(this.recipient.value));
     let destinationAddress = JSON.parse(JSON.stringify(this.destinationAddress.value));
     let pickupWindows = JSON.parse(JSON.stringify(this.articleForm1.value.products));
     let parcels = JSON.parse(JSON.stringify(this.articleForm.value.products));
+    let apiKey = JSON.parse(JSON.stringify(this.apiKeyForm.value.apiKey));
 
-    
+
 
     var orderInfo = {
-    
       "origin": {
         "name": "Nils Johansen (Sender)",
-        "address" : originAddress,
+        "address": originAddress,
         "email": "testemail+sender@porterbuddy.com",
         "phoneCountryCode": "+47",
         "phoneNumber": "65127865"
       },
 
-      "destination":  {
+      "destination": {
         "name": "Roger Olsen (Recipient)",
         "address": destinationAddress,
-       "email" : recipientAdress.email,
-       "phoneCountryCode" : recipientAdress.phoneCountryCode,
-       "phoneNumber" : recipientAdress.phoneNumber,
+        "email": recipientAdress.email,
+        "phoneCountryCode": recipientAdress.phoneCountryCode,
+        "phoneNumber": recipientAdress.phoneNumber,
 
         "deliveryWindow": this.deliveryWindow,
         "verifications": {
@@ -196,330 +201,153 @@ saveData : any;
           "idCheck": true,
           "requireSignature": false,
           "onlyToRecipient": true
-      }
-    },
-      "parcels":    parcels,
+        }
+      },
+      "parcels": parcels,
 
       "product": "delivery",
       "courierInstructions": "Test",
       "shipmentIdentifier": "12345"
 
-      }
-    
-      var x = Math.random();
+    }
 
-      console.log(orderInfo);
-      let header = new HttpHeaders();
-      header = header.append('content-type', 'application/json');
-      header = header.append('Access-Control-Allow-Origin', '*');
-      header = header.append('Access-Control-Allow-Methods', 'POST');
-      header = header.append('x-api-key', 'yOxaPhpechYuey1mi16JZNI4p4laved0LKuOw8KS');
-      header = header.append('Idempotency-Key', x.toString());
+    var randomINT = Math.random();
+    let header = new HttpHeaders();
+    header = header.append('content-type', 'application/json');
+    header = header.append('Access-Control-Allow-Origin', '*');
+    header = header.append('Access-Control-Allow-Methods', 'POST');
+    header = header.append('x-api-key', apiKey);
+    header = header.append('Idempotency-Key', randomINT.toString());
 
+    var orderToString = JSON.stringify(orderInfo);
 
+    this.http.post("https://corsporter.herokuapp.com/https://api.porterbuddy-test.com/order", orderToString, { headers: header }
+    )
+      .subscribe(
+        data => {
+          this.showIframe1 = true;
+          this.selectedBool = false;
+          this.orderID = (data as any).orderId
+          this.showIframe = false;
+          this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl((data as any)._links.userInformation.href);
+        },
+        error => {
+          this.showIframe1 = false;
 
-      const headers1 = {
-        "x-api-key": "yOxaPhpechYuey1mi16JZNI4p4laved0LKuOw8KS", // your public token
+          this.myObj = error.error[0];
 
-        "Content-Type": "application/json",
-     
-        'Idempotency-Key': x.toString(),
+        }
 
-
-    
-        // Public token must be sed on frontend side, for backend integration use your api key and header
-        // "x-api-key" as specified in the API documentation
-      }
-    
-      var orderInfosec = JSON.stringify(orderInfo);
-
-      
-      const response =  fetch('https://api.porterbuddy-test.com/order', {
-        method: "POST",
-        headers: headers1,
-        body: orderInfosec,
-        credentials: 'include',
-      })
-      .then(response => response.json())
-      .then(data => {
-   
-    
-       
-      })
-      .catch((error) => {
-        this.avError = false;
-        this.showErrorDataBool = true;
-        
-        this.showIframe = false;
-        this.errorData = {"Messege": "Something whent wrong please check error"};
-      });
-    
-      
-      
-      
-      console.log(orderInfosec);
-     
-      this.http.post("https://api.porterbuddy-test.com/order", orderInfosec, { headers: header }
-      )
-        .subscribe(
-          data => {
-            this.showIframe1 = true;
-            this.selectedBool = false;
-            this.orderID = (data as any).orderId
-           console.log((data as any)._links.userInformation.href);
-           this.showIframe = false;
-           this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl((data as any)._links.userInformation.href);
-
-          },
-          error => {
-            this.showIframe1 = false;
-
-            this.myObj = error.error[0];
-            
-          }
-      
-        );
-
-  
-
-  
-
+      );
 
   }
-  avabilityReqest(){
+  avabilityReqest() {
     this.showIframe = false;
     this.showIframe1 = false;
     this.titlePB = "Update Checkout"
-    this.avError =true;
-    console.log("asds");
-
+    this.avError = true;
     let originAddress = JSON.parse(JSON.stringify(this.originAddress.value));
     let recipientAdress = JSON.parse(JSON.stringify(this.recipient.value));
     let destinationAddress = JSON.parse(JSON.stringify(this.destinationAddress.value));
     let pickupWindows = JSON.parse(JSON.stringify(this.articleForm1.value.products));
     let parcels = JSON.parse(JSON.stringify(this.articleForm.value.products));
+    let publicToken = JSON.parse(JSON.stringify(this.apiKeyForm.value.publicToken));
 
 
+    var dataCheckout = {
 
-
-    var dataCheckout1 = {
-    
-        "pickupWindows": pickupWindows,
-        "originAddress": originAddress,
-        "destinationAddress": destinationAddress,
-        "recipient": recipientAdress,
-        "products": [ "delivery" ],
-        "parcels":    parcels,
-
-    
-
+      "pickupWindows": pickupWindows,
+      "originAddress": originAddress,
+      "destinationAddress": destinationAddress,
+      "recipient": recipientAdress,
+      "products": ["delivery"],
+      "parcels": parcels,
     }
 
-let header = new HttpHeaders();
-header = header.append('x-api-key', 'yOxaPhpechYuey1mi16JZNI4p4laved0LKuOw8KS');
-header = header.append('content-type', 'application/json');
-header = header.append('Access-Control-Allow-Methods', 'POST');
-header = header.append('Access-Control-Allow-Origin', 'https://api.porterbuddy-test.com/availability');
-header = header.append('Access-Control-Allow-Credentials', 'true');
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    "content-type": "application/json",
-    "accept":"application/json",
-    "x-public-token": "yOxaPhpechYuey1mi16JZNI4p4laved0LKuOw8KS",
-    'Access-Control-Allow-Origin': 'api.porterbuddy-test.com'
+    var dataCheckoutToJson = JSON.stringify(dataCheckout);
 
-  }),
-  withCredentials: true,
-  secure: false
-};
+    const headers = {
+      "content-type": "application/json",
+      "accept": "application/json",
+      "x-public-token": publicToken, // your public token
 
-var dataCheckoutToJson1 = JSON.stringify(dataCheckout1);
-console.log(dataCheckoutToJson1);
-
-
-
-  const headers = {
-    "content-type": "application/json",
-    "accept":"application/json",
-    "x-public-token": "PeVR7JvLZSKcbQWolrQvKj3vdxEE1B6kRTixeTjM", // your public token
-
-    // Public token must be sed on frontend side, for backend integration use your api key and header
-    // "x-api-key" as specified in the API documentation
-  }
-
- 
-
-  // use test or production URL depending on environment
-  const response =  fetch('https://api.porterbuddy-test.com/availability', {
-    method: "POST",
-    headers: headers,
-    body: dataCheckoutToJson1
-  })
-  .then(response => response.json())
-  .then(data => {
-
-
-    this.saveData = data;
-    this.checkoutObj = dataCheckout1;
-    this.myObj = {};
-
-   (window as any).porterbuddy = {  
-    token: "PeVR7JvLZSKcbQWolrQvKj3vdxEE1B6kRTixeTjM", // your public token
-    view: "checkout",
-    availabilityResponse: data,
-    initialSelectedWindow	 : (data as any).deliveryWindows[0],
-
-
-  //  initialSelectedWindow	 : availabilityResponse.deliveryWindows[3],
-
-    onSetCallbacks: function (callbacks) {
-      (window as any).forceRefreshReference = callbacks.forceRefresh;
-      (window as any).unselectDeliveryWindow = callbacks.unselectDeliveryWindow;
-      (window as any).setSelectedDeliveryWindow = callbacks.setSelectedDeliveryWindow;
-  },
-
-
-  onUpdateDeliveryWindows: (callback, additionalInfo) => {
-    console.log("heej");
-    console.log(this.saveData);
-    (window as any).forceRefreshReference();
-
-      callback(this.saveData.deliveryWindows)
-      console.log((window as any).porterbuddy);
-  },
-
-
-    onSelectDeliveryWindow: (deliveryWindow) => {
-      console.log(deliveryWindow);
-      this.selectedBool = true;
-      console.log("now");
-     this.deliveryWindow = deliveryWindow;
+      // Public token must be sed on frontend side, for backend integration use your api key and header
+      // "x-api-key" as specified in the API documentation
     }
-    
-
-    
-  }
-  this.avError = true;
-  this.showErrorDataBool = false;
-  console.log("bamse")
-  if(this.hej > 1){
-  this.hej1();
-  }
-  this.hej = 2;
-    
-    console.log('Success:', data);
-  })
-  .catch((error) => {
-    this.avError = false;
-    this.showErrorDataBool = true;
-    
-    this.showIframe = false;
-    this.errorData = {"Messege": "Something whent wrong please check error"};
-  });
-
-  
-
-}
-
-hej = 1;
-  
-hej1(){
-
-  console.log("asd");
-  (window as any).forceRefreshReference();
-
-}
 
 
-  bajs(){
-    var data = JSON.stringify({
-      "origin": {
-        "name": "Nils Johansen (Sender)",
-        "address": {
-          "streetName": "Keysers Gate",
-          "streetNumber": "3",
-          "postalCode": "0165",
-          "city": "Oslo",
-          "country": "Norway"
-        },
-        "email": "stefan@porterbuddy.com",
-        "phoneCountryCode": "+47",
-        "phoneNumber": "65127865",
-        "pickupWindows": [
-          {
-            "start": "2021-05-28T17:30:00+02:00",
-            "end": "2021-05-28T19:30:00+02:00"
+    this.http.post("https://corsporter.herokuapp.com/https://api.porterbuddy-test.com/availability", dataCheckoutToJson, { headers: headers }
+    )
+      .subscribe(
+        data => {
+
+          this.saveData = data;
+          this.checkoutObj = dataCheckout;
+          this.myObj = {};
+
+          (window as any).porterbuddy = {
+            token: publicToken, // your public token
+            view: "checkout",
+            availabilityResponse: data,
+            initialSelectedWindow: (data as any).deliveryWindows[0],
+
+
+            //  initialSelectedWindow	 : availabilityResponse.deliveryWindows[3],
+
+            onSetCallbacks: function (callbacks) {
+              (window as any).forceRefreshReference = callbacks.forceRefresh;
+              (window as any).unselectDeliveryWindow = callbacks.unselectDeliveryWindow;
+              (window as any).setSelectedDeliveryWindow = callbacks.setSelectedDeliveryWindow;
+            },
+
+
+            onUpdateDeliveryWindows: (callback, additionalInfo) => {
+              (window as any).forceRefreshReference();
+
+              callback(this.saveData.deliveryWindows)
+            },
+
+
+            onSelectDeliveryWindow: (deliveryWindow) => {
+              this.selectedBool = true;
+              this.deliveryWindow = deliveryWindow;
+            }
           }
-        ]
-      },
-      "destination": {
-        "name": "Mats Olsen (Recipient)",
-        "address": {
-          "streetName": "Høyenhallveien",
-          "streetNumber": "25",
-          "postalCode": "0678",
-          "city": "Oslo",
-          "country": "Norway"
+          this.avError = true;
+          this.showErrorDataBool = false;
+          if (this.hej > 1) {
+            this.hej1();
+          }
+          this.hej = 2;
+
+
         },
-        "email": "stefan@porterbuddy.com",
-        "phoneCountryCode": "+47",
-        "phoneNumber": "700911516",
-        "deliveryWindow": {
-          "start": "2021-12-02T19:30:00+01:00",
-          "end": "2021-12-02T21:30:00+01:00",
-          "token": "E0LyQuF/rQSHCuyXIh6mfQ==:eIbBL7D4aIECETdfNQwJ2g==:iEa4BrDuKeoQCnbt4J/gCgyf+NHpl3XAKr2Wyv9/r0c0FUk0veWkp/cjrOwIJg+qquTgA/+2je3qqdzLkeJgGMT1N5rZlxzKsQsq/jMKcZbAU2lmiAFaKa5B8qm21aFFYXgWAymPS60pPAD+Dyg/eg=="
-        },
-        "verifications": {
-          "minimumAgeCheck": 16,
-          "leaveAtDoorstep": false,
-          "idCheck": true,
-          "requireSignature": false,
-          "onlyToRecipient": true
+        error => {
+          this.avError = false;
+          this.showErrorDataBool = true;
+
+          this.showIframe = false;
+          this.errorData = error.error;
         }
-      },
-      "parcels": [
-        {
-          "description": "Shoes",
-          "widthCm": 1,
-          "heightCm": 40,
-          "depthCm": 1,
-          "weightGrams": 2000,
-          "parcelShipmentIdentifier": "12345"
-        },
-        {
-          "description": "Shoes",
-          "widthCm": 1,
-          "heightCm": 40,
-          "depthCm": 1,
-          "weightGrams": 2000,
-          "parcelShipmentIdentifier": "12346"
-        }
-      ],
-      "product": "delivery",
-      "courierInstructions": "Test",
-      "shipmentIdentifier": "1192345"
-    });
-    
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    
-    xhr.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        console.log(this.responseText);
-      }
-    });
-    
-    xhr.open("POST", "https://api.porterbuddy-test.com/order");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("x-api-key", "yOxaPhpechYuey1mi16JZNI4p4laved0LKuOw8KS");
-    xhr.setRequestHeader("Idempotency-Key", "yOxaPhpechYuesy1mi16JZNI4p4laved0LKuOw8KSdde23xxxxxxxxxxxxxxxxxx");
-    
-    xhr.send(data);
+
+      );
+
   }
+
+  hej = 1;
+
+  hej1() {
+
+    // (window as any).forceRefreshReference();
+
+  }
+
+
 
 
   ngOnInit() {
+
     this.cartDataForm = this.formBuilder.group({
       shipping: [15, Validators.required],
       shippingtax: [1.25, Validators.required],
@@ -554,44 +382,51 @@ hej1(){
       privacyPolicy: ['https://yourclient.com/terms', Validators.required]
     });
 
-  
+
 
     this.pickupWindows = this.formBuilder.group({
       start: ["2021-11-13T10:00+01:00", Validators.required],
       end: ["2021-11-13T20:00+01:00", Validators.required],
- 
+
     });
 
     this.parcels = this.formBuilder.group({
       widthCm: ['30', Validators.required],
       heightCm: ['25', Validators.required],
-      depthCm  : ['45', Validators.required],
-      weightGrams  : ['2000 ', Validators.required],
+      depthCm: ['45', Validators.required],
+      weightGrams: ['2000 ', Validators.required],
 
-    
+
     });
 
     this.recipient = this.formBuilder.group({
       email: ['testemail+recipient@porterbuddy.com', Validators.required],
       phoneCountryCode: ['+47', Validators.required],
-      phoneNumber  : ['65127865', Validators.required],
-    
+      phoneNumber: ['65127865', Validators.required],
+
     });
 
 
     this.originAddress = this.formBuilder.group({
       streetName: ['Keysers Gate', Validators.required],
       streetNumber: ['3', Validators.required],
-      postalCode  : ['0165', Validators.required],
+      postalCode: ['0165', Validators.required],
       city: ['Oslo', Validators.required],
       country: ['Norway', Validators.required]
     });
+
+    this.apiKeyForm = this.formBuilder.group({
+      apiKey: ['yOxaPhpechYuey1mi16JZNI4p4laved0LKuOw8KS', Validators.required],
+      publicToken: ['PeVR7JvLZSKcbQWolrQvKj3vdxEE1B6kRTixeTjM', Validators.required],
+
+    });
+
 
 
     this.destinationAddress = this.formBuilder.group({
       streetName: ['Høyenhallveien Gate', Validators.required],
       streetNumber: ['24', Validators.required],
-      postalCode  : ['0678', Validators.required],
+      postalCode: ['0678', Validators.required],
       city: ['Oslo', Validators.required],
       country: ['Norway', Validators.required]
     });
@@ -603,7 +438,7 @@ hej1(){
       ])
     });
 
-    
+
     this.articleForm1 = this.formBuilder.group({
       products: this.formBuilder.array([
         this.initArticle1()
@@ -641,6 +476,7 @@ hej1(){
       pno: ['195501011018', Validators.required],
     }
     );
+    this.avabilityReqest();
 
   }
 
@@ -680,15 +516,14 @@ hej1(){
     });
   }
 
-   x = 14;
-   
+  x = 14;
+
   initArticle1() {
     this.x = this.x + 1;
-    console.log(this.x);
     return this.formBuilder.group({
-      start: this.formBuilder.control('2021-09-'+this.x.toString()+'T10:00+01:00', Validators.required),
-      end: this.formBuilder.control('2021-09-'+this.x.toString()+'T18:00+01:00', Validators.required),
-  
+      start: this.formBuilder.control('2021-09-' + this.x.toString() + 'T10:00+01:00', Validators.required),
+      end: this.formBuilder.control('2021-09-' + this.x.toString() + 'T18:00+01:00', Validators.required),
+
     });
   }
 
